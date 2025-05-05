4e46cea2d963a9113ba2fbb2c8c97c9792ac28d4
wrightbradley <bradley.wright.tech@gmail.com>
2025-05-05 19:21:28 -0400

    ci: update and run pre-commit
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,1 +1,4 @@
-kubectl get pods -o wide -A | grep -v cloudnative-pg | grep ago | awk '{print $1, $2}' | while read namespace pod; do kubectl delete pod "$pod" -n "$namespace"; done
+#!/bin/bash
+
+# kubectl get pods -o wide -A | grep -v cloudnative-pg | grep ago | awk '{print $1, $2}' | while read namespace pod; do kubectl delete pod "$pod" -n "$namespace"; done
+kubectl get pods -o wide -A | grep -v cloudnative-pg | grep -v '1/1' | grep -v '2/2' | grep -v '3/3' | grep -v '4/4' | awk '{print $1, $2}' | while read namespace pod; do kubectl delete pod "$pod" -n "$namespace"; done
